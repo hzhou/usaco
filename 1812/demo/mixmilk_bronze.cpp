ad0cf740231c3b00721ce30a4e67e34bf6e656c9
Hui Zhou <hzhou@hzsolutions.net>
2019-06-18 20:30:32 -0500

    update

---
+++ 1812/demo/mixmilk_bronze.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int main(){
+    // printf("Hello World!\n");
+    FILE * In = fopen("mixmilk.in", "r");
+    int A, B, C, a, b, c;
+    fscanf(In, "%d %d", &A, &a);
+    fscanf(In, "%d %d", &B, &b);
+    fscanf(In, "%d %d", &C, &c);
+    fclose(In);
+
+    // printf("A is %d - %d, B is %d - %d, C is %d - %d\n", A, a, B, b, C, c);
+    int count = 0;
+    while (count < 100 ){
+        if (b+a <= B){
+            b += a;
+            a = 0;
+        }
+        else {
+            a = (a+b) - B ;
+            b = B;
+        }
+        count ++;
+        if(count >= 100) {
+            break;
+        }
+        // ----------------
+        if (c+b <= C){
+            c += b;
+            b = 0;
+        }
+        else {
+            b = (b+c) - C ;
+            c = C;
+        }
+        count ++;
+        if(count >= 100) {
+            break;
+        }
+        // ----------------
+        if (a+c <= A){
+            a += c;
+            c = 0;
+        }
+        else {
+            c = (c+a) - A ;
+            a = A;
+        }
+        count ++;
+    }
+
+    FILE * Out = fopen("mixmilk.out", "w");
+    fprintf(Out, "%d\n%d\n%d\n", a, b, c);
+    fclose(Out);
+}
