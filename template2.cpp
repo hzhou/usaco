fa71152064becf02e993d45b386e3f044914610e
Jeffrey Huang <jeffreyhuang165@gmail.com>
2019-09-08 10:21:30 -0500

    Add files via upload

---
+++ template2.cpp
@@ -0,0 +1,51 @@
+/*
+ * ID: jhuang1
+ * PROG: sample
+ * LANG: C++11
+ * RESULT: 8/10
+ * TIME: 1ms
+ * COMMENT: test1
+ * */
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <climits>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <chrono>
+#include <numeric>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	const char* FIN  = "sample.in";
+	const char* FOUT = "sample.out";
+	const char* inFile = argc > 1 ? argv[1] : FIN;
+	ifstream fin(inFile);
+	ofstream fout(FOUT);
+
+	//////////////////////////////////////
+	// Start your solution from here...
+
+
+
+	// End of your soulution.
+	//////////////////////////////////////
+
+	return 0;
+}
+
+/*
+Instructions:
+git add dining_gold.cpp
+git commit
+git push
+*/
+
