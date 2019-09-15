fccb983d90460b7f34c8a002563c8911ccb5098c
jhuang165 <jeffreyhuang165@gmail.com>
2019-09-15 09:50:41 -0500

    oh hello there

--- 1812/Jeffrey/dining.cpp
+++ 1812/Jeffrey/dining.cpp
@@ -15,6 +15,8 @@
 #include <vector>
 
 using namespace std;
+
+typdef make_pair(a, b) mp(a, b);
  
 int n, m, k, hay[100000], y[100000];
 vector<int> neighbors[100001];
@@ -22,40 +24,40 @@ map<pair<int,int>, int> val;
 map<int,int> dist; 
 set<pair<int,int>> visited;	
  
-void spth(int s) {
-	visited.clear();
-  	visited.insert(make_pair(0, s));
-  	while (!visited.empty()) {
-    	int i = visited.begin()->second;
-    	visited.erase(visited.begin());
-    	for (auto j : neighbors[i]) {
-      		if (dist.count(j) == 0 || dist[i] + val[make_pair(i, j)] < dist[j]) {
-				dist[j] = dist[i] + val[make_pair(i, j)];
-				visited.insert(make_pair(dist[j], j));
-    		}
-    	}
-    }
-}
- 
 int main() {
+    // INPUT
   	ifstream fin("dining.in");
   	fin >> n >> m >> k;
   	int a, b, t;
   	for (int i = 0; i < m; ++i) {
-    	fin >> a >> b >> t;
-    	a--; 
-    	b--;
-    	neighbors[a].push_back(b);
-    	neighbors[b].push_back(a);
-    	val[make_pair(a, b)] = t;
-    	val[make_pair(b, a)] = t;
+        fin >> a >> b >> t;
+    	neighbors[a - 1].push_back(b - 1);
+    	neighbors[b - 1].push_back(a - 1);
+    	val[make_pair(a - 1, b - 1)] = t;
+    	val[make_pair(b - 1, a - 1)] = t;
   	}
   	for (int i = 0; i < k; ++i) {
     	fin >> hay[i] >> y[i];
-    	hay[i]--;
   	}
 
-  	spth(n - 1);
+    // ALGORITHM
+
+    for (int i = 0; i < k; ++i) {
+        hay[i]--;
+    }
+
+  	visited.clear();
+    visited.insert(make_pair(0, n - 1));
+    while (!visited.empty()) {
+        int i = visited.begin()->second;
+        visited.erase(visited.begin());
+        for (auto j : neighbors[i]) {
+            if (dist.count(j) == 0 || dist[i] + val[make_pair(i, j)] < dist[j]) {
+                dist[j] = dist[i] + val[make_pair(i, j)];
+                visited.insert(make_pair(dist[j], j));
+            }
+        }
+    }
 
   	map<int, int> odist = dist;
 
@@ -65,7 +67,20 @@ int main() {
   	}
 
   	dist.clear();
-  	spth(n);
+  	visited.clear();
+    visited.insert(make_pair(0, n));
+    while (!visited.empty()) {
+      int i = visited.begin()->second;
+      visited.erase(visited.begin());
+      for (auto j : neighbors[i]) {
+          if (dist.count(j) == 0 || dist[i] + val[make_pair(i, j)] < dist[j]) {
+        dist[j] = dist[i] + val[make_pair(i, j)];
+        visited.insert(make_pair(dist[j], j));
+        }
+      }
+    }
+
+    // OUTPUT
 
   	ofstream fout("dining.out");
   	for (int i = 0; i < n - 1; ++i) {
