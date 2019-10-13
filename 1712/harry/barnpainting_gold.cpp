d7acb2381906ecb0ab2eb9d5a7bb2a0cfabd44b0
Harry Zhou <harry@hz2.org>
2019-10-12 20:49:39 -0500

    modified:   barnpainting_gold.cpp

--- 1712/harry/barnpainting_gold.cpp
+++ 1712/harry/barnpainting_gold.cpp
@@ -1,101 +1,148 @@
 //Harry Zhou
-//50 mins
-//First time I made a submission, I neglected to add caching, but it was a quick fix.
-#define scanN() fscanf(fileIn, "%d\n", &N);
-#define scanm(identifiers, ptrs...) fscanf(fileIn, #identifiers, ptrs);
-#define scand(ptr) fscanf(fileIn, "%d\n", ptr);
-#define scanld(ptr) fscanf(fileIn, "%ld\n", ptr);
-#define scans(ptr) fscanf(fileIn, "%s\n", ptr);
-#define scanmA(amount, identifiers, ptrs...) for (int i=0; i<amount; i++) {fscanf(fileIn, #identifiers, ptrs);}
-#define scandA(amount, ptr) for (int i=0; i<amount; i++) {fscanf(fileIn, "%d", ptr);}
-#define forN(amount) for (int i=0; i<amount; i++)
-#define MOD 1000000007;
-
-#include <cstdio>
+//1hr 30min
+//10-12-2019
+//Took a while to realize that iterating up from the leaf nodes is not topological ordering
+#include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <string.h>
+#include <math.h>
 #include <algorithm>
 #include <limits.h>
 #include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
 
-struct barn {
-    std::vector<int> connections;
-    int poss[3];
-    int fixedCol;
-    int parent;
-};
+#define MOD 1000000007
 
-barn *barns;
+using std::cout;
+using std::endl;
+using std::make_pair;
 
-int64_t getPoss(int n, int col);
+using std::pair;
+using std::set;
+using std::map;
+using std::vector;
+using std::stack;
+using std::priority_queue;
 
-int main() {
-    FILE *fileIn = fopen("barnpainting.in", "r");
-    FILE *fileOut = fopen("barnpainting.out", "w");
+typedef long long int LL;
 
-    int N, K;
-    scanm(%d %d, &N, &K);
-    barns = new barn[N];
-    for (int i=0; i<N-1; i++) {
-        int x, y;
-        scanm(%d %d, &x, &y);
-        x--; y--;
-        barns[x].connections.push_back(y);
-        barns[y].connections.push_back(x);
-    }
+typedef pair<int,int> ipair;
+typedef set<int> iset;
+typedef map<int,int> imap;
+typedef vector<int> ivec;
+typedef stack<int> istack;
+typedef priority_queue<ipair, std::vector<ipair>, std::greater<ipair>> dijkstra_priority_queue;
 
-    for (int i=0; i<N; i++) {
-        barns[i].poss[0] = -1;
-        barns[i].poss[1] = -1;
-        barns[i].poss[2] = -1;
-        barns[i].fixedCol = 0;
-    }
-
-    for (int i=0; i<K; i++) {
-        int b, c;
-        scanm(%d %d, &b, &c);
-        b--;
-        barns[b].fixedCol = c;
-    }
-
-    int64_t poss = getPoss(0, -1);
-    printf("%ld\n", poss);
-    fprintf(fileOut, "%ld\n", poss);
+int ladd(LL a, LL b) {
+    return (int) ((a+b) % MOD);
 }
 
-int64_t getPoss(int n, int pcol) {
-    //printf("%d\n", n);
-    int64_t tot = 0;
-    if (barns[n].fixedCol == pcol) {
-        return 0;
+int lmul(LL a, LL b) {
+    return (int) (a*b % MOD);
+}
+
+int main() {
+    std::ifstream fin;
+    fin.open("barnpainting.in");
+    std::ofstream fout;
+    fout.open("barnpainting.out");
+
+    int N, K;
+    fin >> N >> K;
+    ivec *graph = new ivec[N];
+    for (int i=0; i<N-1; i++) {
+        int a, b;
+        fin >> a >> b;
+        a--; b--;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
     }
-    for (int col = 1; col <= 3; col++) {
-        int64_t mult = 1;
-        if (barns[n].fixedCol) {
-            col = barns[n].fixedCol;
-        }
-        if (col == pcol) {continue;}
-        if (barns[n].poss[col-1] == -1) {
-            for (auto itr = barns[n].connections.begin(); itr != barns[n].connections.end(); itr++) {
-                if (*itr == barns[n].parent) {
-                    continue;
-                }
-                else {
-                    barns[*itr].parent = n;
-                    mult *= getPoss(*itr, col);
-                    mult %= MOD;
-                }
+
+    int *fc = new int[N];
+    for (int i=0; i<N; i++) {fc[i] = 0;}
+    for (int i=0; i<K; i++) {
+        int b, c;
+        fin >> b >> c;
+        b--;
+        fc[b] = c;
+    }
+
+    int *tree = new int[N];
+    for (int i=0; i<N; i++) {tree[i]=-1;}
+    istack stk;
+    tree[0] = 0;
+    stk.push(0);
+    while (!stk.empty()) {
+        int v = stk.top();
+        stk.pop();
+        for (auto itr = graph[v].begin(); itr!= graph[v].end(); itr++) {
+            if (tree[*itr] == -1) {
+                tree[*itr] = v;
+                stk.push(*itr);
             }
-            barns[n].poss[col-1] = mult;
-        }
-        else {
-            mult = barns[n].poss[col-1];
-        }
-        tot += mult;
-        tot %= MOD;
-        if (barns[n].fixedCol) {
-            break;
         }
     }
-    return tot;
+
+    int *c1 = new int[N];
+    int *c2 = new int[N];
+    int *c3 = new int[N];
+    for (int i=0; i<N; i++) {
+        c1[i] = 1;
+        c2[i] = 1;
+        c3[i] = 1;
+    }
+
+    istack ord_stk;
+
+    int *visited = new int[N];
+    for (int i=0; i<N; i++) {visited[i]=0;}
+
+    for (int i=0; i<N; i++) {
+        if (visited[i]) {continue;}
+        visited[i] = 1;
+        stk.push(i);
+        while (!stk.empty()) {
+            int n = stk.top();
+            if (!visited[tree[n]]) {
+                visited[tree[n]] = 1;
+                stk.push(tree[n]);
+            }
+            else {
+                ord_stk.push(n);
+                stk.pop();
+            }
+        }
+    }
+
+    while (!ord_stk.empty()) {
+        int node = ord_stk.top();
+        ord_stk.pop();
+        int parent = tree[node];
+        if (fc[node] == 1) {
+            c2[node] = 0;
+            c3[node] = 0;
+        }
+        else if (fc[node] == 2) {
+            c1[node] = 0;
+            c3[node] = 0;
+        }
+        else if (fc[node] == 3) {
+            c1[node] = 0;
+            c2[node] = 0;
+        }
+
+        if (node == 0) {break;}
+
+        c1[parent] = lmul( c1[parent] , ladd(c2[node],c3[node]) );
+        c2[parent] = lmul( c2[parent] , ladd(c1[node],c3[node]) );
+        c3[parent] = lmul( c3[parent] , ladd(c1[node],c2[node]) );
+    }
+
+    int ans = ladd( ladd(c1[0],c2[0]) , c3[0] );
+    cout << ans << endl;
+    fout << ans << endl;
 }
