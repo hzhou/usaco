3012e4a61d45e1bd50af7ed1d65b413eace170ec
Brian Lu <lu_haoqin@outlook.com>
2019-11-01 23:01:31 -0500

    Create lemonade_silver.cpp

---
+++ 18open/brian/lemonade_silver.cpp
@@ -0,0 +1,46 @@
+// Brian Lu
+//1. Sort
+//16 mins
+//
+//***x**x***
+
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <limits>
+
+bool compare(int a, int b) { 
+  return a > b;
+} 
+int main() {
+
+  int N;
+  FILE *input = fopen("lemonade.in","r");
+  fscanf(input, "%d", &N);
+  int cows[N];
+  for (int i = 0; i < N; i++) {
+    fscanf(input, "%d", &cows[i]);
+  }
+  fclose(input);
+
+  std::sort(cows, cows+(sizeof(cows)/sizeof(cows[0])), compare);
+
+  int line = 0;
+
+  for (int i = 0; i < N; i++) {
+    if (cows[i] >= line) {
+      line++;
+    } else {
+      break;
+    }
+  }
+
+  FILE *output = fopen("lemonade.out", "w");
+  fprintf(output, "%d", line);
+  fclose(output);
+  
+  return 0;
+}
