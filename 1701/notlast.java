d9319447af35c9f5875b78a056e51cf66d47f9a6
meyang1 <52054330+meyang1@users.noreply.github.com>
2019-11-16 13:26:53 -0600

    Update notlast.java

--- 1701/notlast.java
+++ 1701/notlast.java
@@ -9,35 +9,46 @@ public class notlast{
                 int N = scan.nextInt();
                 int[] Cows = new int[7];
                 String name = "";
+		int milk=0;
                 for(int i=0; i<N; i++){
                     name = scan.next();
-                    if(name.equals("Bessie")) Cows[0]++;
-                    if(name.equals("Elsie")) Cows[1]++;               
-                    if(name.equals("Daisy")) Cows[2]++;                  
-                    if(name.equals("Gertie")) Cows[3]++;                  
-                    if(name.equals("Annabelle")) Cows[4]++;                    
-                    if(name.equals("Maggie")) Cows[5]++;                   
-                    if(name.equals("Henrietta")) Cows[6]++;                  
+		    milk=scan.nextInt();
+                    if(name.equals("Bessie")) Cows[0]+=milk;
+                    else if(name.equals("Elsie")) Cows[1]+=milk;               
+                    else if(name.equals("Daisy")) Cows[2]+=milk;                  
+                    else if(name.equals("Gertie")) Cows[3]+=milk;                  
+                    else if(name.equals("Annabelle")) Cows[4]+=milk;                    
+                    else if(name.equals("Maggie")) Cows[5]+=milk;                   
+                    else Cows[6]+=milk;                  
                 }
                 scan.close();
-                
-                int min=100;
-                for(int i=0; i<7; i++)
-                        if(Cows[i]<min) min=Cows[i];       
-                 
+    		
+		//find min            
+                int min=10000000;
                 for(int i=0; i<7; i++){
-                        if(Cows[i]==min) Cows[i]=101;
-                        
-                min=100;
-                for(int i=0; i<7; i++)
+                        if(Cows[i]<min) min=Cows[i];   
+		    
+                }
+		//take out min
+                for(int i=0; i<7; i++){
+                        if(Cows[i]==min) Cows[i]=1000001;
+                }
+
+		//find 2nd min
+                min=100000;
+                for(int i=0; i<7; i++){
                         if(Cows[i]<min) min=Cows[i];
-                       
-                
+                }       
+		
+		//count 2nd min
+		int count=0;
+		for(int i=0; i<7; i++){
+                        if(Cows[i]==min) count++;
+                }
                 PrintWriter out = new PrintWriter(new BufferedWriter (new FileWriter("notlast.out")));
                 
-                if(min==0||min>1) out.println("Tie");
-                else{
-                        for(int i=0; i<7; i++){
+                if(count==1){
+                	for(int i=0; i<7; i++){
                                 if(Cows[i]==min){
                                         if(i==0)  out.println("Bessie");
                                         if(i==1)  out.println("Elsie");           
@@ -45,9 +56,12 @@ public class notlast{
                                         if(i==3)  out.println("Gertie");              
                                         if(i==4)  out.println("Annabelle");                   
                                         if(i==5)  out.println("Maggie");                  
-                                        if(i==6)  println("Henrietta");        
+                                        if(i==6)  out.println("Henrietta");        
                                 }
-                        }
-                }
-          }
- }
+                	}
+		
+		}
+		else{
+			out.println("Tie");
+		}
+		out.close();
